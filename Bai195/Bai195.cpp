dc2fe11e9e8e3c75a17fa96fd9f7623131ba61b6
HuynhDienThuc1 <23521555@gm.uit.edu.vn>
2023-11-14 16:53:47 +0700

    thuc

---
+++ Bai195/Bai195.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void Nhap(int[][100], int&, int&);
+void Xuat(int[][100], int, int);
+bool ktNguyenTo(int);
+void LietKe(int[][100], int, int);
+
+
+int main()
+{
+	int b[100][100];
+	int k,l;
+	Nhap(b, k,l);
+
+	cout << "Ma tran ban dau:";
+	Xuat(b, k,l);
+
+	cout << "\nCac so nguyen to: ";
+	LietKe(b, k, l);
+
+	cout << "\n\n\nKet thuc!!!";
+	return 0;
+}
+
+void Nhap(int a[][100], int& m, int& n)
+{
+	cout << "Nhap so dong: ";
+	cin >> m;
+	cout << "Nhap so hang: ";
+	cin >> n;
+	srand(time(NULL));
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+			a[i][j] = -100 + rand() / ((int)RAND_MAX / 200);
+}
+
+void Xuat(int a[][100], int m, int n)
+{
+	if (m == 0)
+		return;
+	Xuat(a, m - 1, n);
+	cout << endl;
+	for (int j = 0; j < n; j++)
+		cout << setw(4)  << a[m - 1][j];
+}
+
+bool ktNguyenTo(int k)
+{
+	int dem = 0;
+	for (int i = 1; i <= k; i++)
+		if (k % i == 0)
+			dem++;
+	return (dem == 2);
+
+}
+
+void LietKe(int a[][100], int m, int n)
+{
+	if (m == 0)
+		return;
+	LietKe(a, m - 1, n);
+	for (int i = 0; i < n; i++)
+		if (ktNguyenTo(a[m - 1][i]))
+			cout <<setw(4)<< a[m - 1][i];
+}
